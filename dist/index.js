52f2b995c46fdcbe3a9081f5bb9686cd56427646
mr cod3r <mrcod3r.ir@gmail.com>
2022-05-07 09:52:46 +0430

    25:57

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,39 @@
 "use strict";
+// Basic Types
 var id = 5;
-console.log("ID is :", id);
+var company = "Traversy Media";
+var isPublished = true;
+var x = "Hello";
+var ids = [1, 2, 3, 4, 6];
+var arr = [1, true, "Hello"];
+// Tuple
+var person = [1, "Brad", true];
+// Tuple Array
+var employee;
+employee = [
+    [1, "Brad"],
+    [2, "John"],
+    [3, "Jill"]
+];
+// Union
+var pid = "22";
+// Enum
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 1] = "Up";
+    Direction1[Direction1["Down"] = 2] = "Down";
+    Direction1[Direction1["Left"] = 3] = "Left";
+    Direction1[Direction1["Right"] = 4] = "Right";
+})(Direction1 || (Direction1 = {}));
+// console.log(Direction1.Down)
+var Direction2;
+(function (Direction2) {
+    Direction2["Up"] = "up";
+    Direction2["Down"] = "down";
+    Direction2["Left"] = "left";
+    Direction2["Right"] = "right";
+})(Direction2 || (Direction2 = {}));
+var user = {
+    id: 1,
+    name: "John"
+};
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,53 @@
-let id: number = 5
+// Basic Types
 
-console.log("ID is :", id)
+let id: number = 5
+let company: string = "Traversy Media"
+let isPublished: boolean = true
+let x: any = "Hello"
+
+let ids: number[] = [1, 2, 3, 4, 6]
+let arr: any[] = [1, true, "Hello"]
+
+// Tuple
+let person: [number, string, boolean] = [1, "Brad", true]
+
+// Tuple Array
+let employee: [number, string][]
+
+employee = [
+	[1, "Brad"],
+	[2, "John"],
+	[3, "Jill"]
+]
+
+// Union
+let pid: string | number = "22"
+
+// Enum
+
+enum Direction1 {
+	Up = 1,
+	Down,
+	Left,
+	Right
+}
+// console.log(Direction1.Down)
+enum Direction2 {
+	Up = "up",
+	Down = "down",
+	Left = "left",
+	Right = "right"
+}
+// console.log(Direction2.Down)
+
+// Object
+
+type User = {
+	id: number
+	name: string
+}
+
+const user: User = {
+	id: 1,
+	name: "John"
+}
